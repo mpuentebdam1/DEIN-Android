adb5bcdcd9e5aee75650b350a73c9c3560709db9
mpuentebdam1 <mpuentebdam1@gmail.com>
2024-01-26 22:29:51 +0100

    Base

---
+++ app/src/main/java/com/example/dein_36/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dein_36
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
