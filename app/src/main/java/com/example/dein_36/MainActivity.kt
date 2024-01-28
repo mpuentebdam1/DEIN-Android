2ab08d6d54369130619e98a529ac0fdb5a356db1
mpuentebdam1 <mpuentebdam1@gmail.com>
2024-01-28 20:48:25 +0100

    Añadidos elementos y colores

--- app/src/main/java/com/example/dein_36/MainActivity.kt
+++ app/src/main/java/com/example/dein_36/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.example.dein_36
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dein_36.R
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+
 }
